736f64efc2065c8fb9e5662eaf7bcab211318c4f
Stefano <stefano.41.dev@gmail.com>
2025-11-09 08:39:10 -0500

    sPrimera configuracion de conexion a mongodb

---
+++ src/main/java/com/stefano/CConnetApplication.java
@@ -0,0 +1,13 @@
+package com.stefano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CConnetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CConnetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stefano/CConnetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stefano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CConnetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
